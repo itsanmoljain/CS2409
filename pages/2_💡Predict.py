f22578eb8681fc9a79c933a8e19f2ece9b0f2c90
Anmol Jain <116163247+itsanmoljain@users.noreply.github.com>
2025-04-20 03:32:01 +0530

    Update 2_💡Predict.py

--- pages/2_💡Predict.py
+++ pages/2_💡Predict.py
@@ -88,3 +88,10 @@ try:
             st.success(f"✅ Legitimate Transaction\n\n**Confidence:** {1 - proba:.2f}")
 except:
     st.error("please enter details.")
+st.markdown("""
+<div style='text-align: center; margin: 30px 0;'>
+    <a href="/Overview" target="_self">
+        <button style='padding: 14px 42px; font-size: 20px; background-color: #007BFF; color: white; border: none; border-radius: 10px;'>🔍 Go to Predictor</button>
+    </a>
+</div>
+""", unsafe_allow_html=True)
